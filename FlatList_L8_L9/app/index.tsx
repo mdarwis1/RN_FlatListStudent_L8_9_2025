13bf6bcbaf66bc9b9e9ef9f1dbabdad6929b8f9b
Milla <144734287+mdarwis1@users.noreply.github.com>
2025-09-12 08:11:56 -0500

    created flatList

--- FlatList_L8_L9/app/index.tsx
+++ FlatList_L8_L9/app/index.tsx
@@ -1,14 +1,38 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-import colors from "@/styles/colors";
 import defaultStyles from "@/styles/defaultStyles";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from "react-native";
 
 export default function Index() {
+
+  type dataType ={
+    id: string; // refer to the unique identifier
+    title: string; // text we will show in the list
+  }
+
+  // using all caps because DATA array will not change
+  // during its use
+  const DATA: dataType[] = [
+    {id: '1', title: 'First Item'},
+    {id: '2', title: 'Second Item'},
+    {id: '3', title: 'Third Item'},
+    {id: '4', title: 'Fourth Item'},
+  ];
+
+  // create a simple function telling me what was selected
+  const selectedList = (item: dataType) => {
+    console.log(item.title);
+    setSelectedId(item.id);
+  }
+
+  // add a state var to keep track of which id is selected
+  const[selectedId, setSelectedId] = useState<string>('1');
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +40,16 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({item}) => (
+              <TouchableHighlight onPress={() => selectedList(item)}>
+                <View style={styles.flatListRow}>
+                  <Text>{item.title}</Text>
+                </View>
+              </TouchableHighlight>
+            )} />
         </View>
       </View>
     </View>
@@ -26,6 +59,12 @@ export default function Index() {
 const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
+  },
+  flatListRow: {
+    backgroundColor: 'lightgreen',
+    margin: 8,
+    padding: 8,
+
   },
   titleContainer: {
     marginTop: 5,
